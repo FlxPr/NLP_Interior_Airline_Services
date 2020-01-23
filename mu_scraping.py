1bc094ef33ce48fde1e4746afba0f731d508e45e
Michael Ullah <ullah.michael@gmail.com>
2020-01-23 21:34:35 +0100

    update

--- mu_scraping.py
+++ mu_scraping.py
@@ -8,8 +8,15 @@ browser.get('https://www.tripadvisor.com/Airlines')
 airlines = browser.find_elements_by_class_name('airlineName')
 print([airline.text for airline in airlines])
 
-reviews_buttons = browser.find_elements_by_class_name('review_button ui_button secondary small').get_attribute('href')
-print([a for a in reviews_buttons])
+airline_section = browser.find_elements_by_class_name("review_button.ui_button.secondary.small")
+browser.find_element_by_tag_name('prw_rup prw_airlines_airline_lander_card')
+
+browser.find_elements_by_class_name('review_button ui_button secondary small')
+
+for airline in airlines:
+    print(airline.get_attribute('href'))
+reviews_buttons = browser.find_elements_by_class_name('review_button ui_button secondary small')
+print([a.get_attribute('href') for a in reviews_buttons])
 
 
 search_box.send_keys('ChromeDriver')
