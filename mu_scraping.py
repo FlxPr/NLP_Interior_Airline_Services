0a9cfa6aa766b16bc749046e8fdf2bd85bd241e5
Michael Ullah <ullah.michael@gmail.com>
2020-01-23 18:22:31 +0100

    update

--- mu_scraping.py
+++ mu_scraping.py
@@ -1,14 +1,15 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome(
+browser = webdriver.Chrome(
     '/Users/michaelullah/Documents/DSBA/CS/S2/SuperCase-elevenstrategy/NLP_Interior_Airline_Services/chromedriver')
 
-driver.get('https://www.tripadvisor.com/Airlines')
-time.sleep(5) # Let the user actually see something!
-airlines = driver.find_elements_by_class_name('airlineName')
-
+browser.get('https://www.tripadvisor.com/Airlines')
+airlines = browser.find_elements_by_class_name('airlineName')
 print([airline.text for airline in airlines])
+
+reviews_buttons = browser.find_elements_by_class_name('review_button ui_button secondary small').get_attribute('href')
+print([a for a in reviews_buttons])
 
 
 search_box.send_keys('ChromeDriver')
