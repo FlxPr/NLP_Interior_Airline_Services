a5d94da8012282421d9f15178aab671acbe9f288
Michael Ullah <ullah.michael@gmail.com>
2020-01-27 14:19:32 +0100

    Update tripadvisor

--- Tripadvisor_scraper.py
+++ Tripadvisor_scraper.py
@@ -26,85 +26,87 @@ browser = Chrome(
 # airlines_links.write('\n'.join(set(airline_links)))
 
 
-airlines_links = open('tripadvisor_airlines_links.txt').read().split('\n')
+airlines_links = open('tripadvisor_airlines_links_shortlist.txt').read().split('\n')
 
 
-
-
-
-for batch in range(51):
-    print(batch)
+for link in airlines_links:
     scraped = []
-    for link in airlines_links[12*batch:12*(batch+1)]:
-        browser.get(link)
-        time.sleep(2)
+    browser.get(link)
+    time.sleep(2)
 
-        marker = 0
-        while marker == 0:
+    try:
+        airline = browser.find_element_by_class_name(
+            'flights-airline-review-page-airline-review-header-AirlineDetailHeader__airlineName--2JeT1').text
+    except:
+        airline = ''
 
-            reviews = browser.find_elements_by_class_name('location-review-card-Card__ui_card--2Mri0.'
-                                                          'location-review-card-Card__card--o3LVm.'
-                                                          'location-review-card-Card__section--NiAcw')
+    marker = 0
+    while marker == 0:
 
-            for review in reviews[:5]:
-                print(review)
+        reviews = browser.find_elements_by_class_name('location-review-card-Card__ui_card--2Mri0.'
+                                                      'location-review-card-Card__card--o3LVm.'
+                                                      'location-review-card-Card__section--NiAcw')
 
-                try:
-                    stars_container = review.find_element_by_class_name(
-                        "location-review-review-list-parts-RatingLine__bubbles--GcJvM")
-                    star = stars_container.find_element_by_tag_name('span').get_attribute("class")
-                except:
-                    star = ""
-
-                try:
-                    title_container = review.find_element_by_class_name("location-review-review-list-parts-ReviewTitle"
-                                                                        "__reviewTitle--2GO9Z")
-                    title = title_container.find_element_by_tag_name('span').text
-                except:
-                    title = ""
-
-                try:
-                    comment = review.find_element_by_class_name("location-review-review-list-parts-ExpandableReview"
-                                                                "__reviewText--gOmRC").find_element_by_tag_name('span').text
-                except:
-                    comment = ""
-
-                try:
-                    date = review.find_element_by_class_name(
-                        'social-member-event-MemberEventOnObjectBlock__event_type--3njyv').text
-                except:
-                    date = ""
-
-                try:
-                    contributions = review.find_element_by_class_name('social-member-MemberHeaderStats__bold--3z3qh').text
-                except:
-                    contributions = ""
-
-                print(comment)
-
-                if any([star != "", title != "", comment != "", date != "", contributions != ""]):
-                    scraped.append(
-                        {'star': star, 'title': title, 'comment': comment, 'date': date, 'contributions': contributions})
-
-                time.sleep(1)
+        for review in reviews[:5]:
+            print(review)
 
             try:
-                button_next = browser.find_element_by_class_name('ui_button.nav.next.primary')
-                button_next.click()
-                time.sleep(1)
+                stars_container = review.find_element_by_class_name(
+                    "location-review-review-list-parts-RatingLine__bubbles--GcJvM")
+                star = stars_container.find_element_by_tag_name('span').get_attribute("class")
             except:
-                marker = 1
-    break
+                star = ""
+
+            try:
+                title_container = review.find_element_by_class_name("location-review-review-list-parts-ReviewTitle"
+                                                                    "__reviewTitle--2GO9Z")
+                title = title_container.find_element_by_tag_name('span').text
+            except:
+                title = ""
+
+            try:
+                comment = review.find_element_by_class_name("location-review-review-list-parts-ExpandableReview"
+                                                            "__reviewText--gOmRC").find_element_by_tag_name('span').text
+            except:
+                comment = ""
+
+            try:
+                date = review.find_element_by_class_name(
+                    'social-member-event-MemberEventOnObjectBlock__event_type--3njyv').text
+            except:
+                date = ""
+
+            try:
+                contributions = review.find_element_by_class_name('social-member-MemberHeaderStats__bold--3z3qh').text
+            except:
+                contributions = ""
+
+            print(comment)
+
+            if any([star != "", title != "", comment != "", date != "", contributions != ""]):
+                scraped.append(
+                    {'airline': airline, 'star': star, 'title': title, 'comment': comment, 'date': date, 'contributions': contributions})
+
+            time.sleep(1)
+
+        try:
+            button_next = browser.find_element_by_class_name('ui_button.nav.next.primary')
+            button_next.click()
+            time.sleep(1)
+        except:
+            marker = 1
+
+
+        break
+
+    scraped = pd.DataFrame(scraped)
+    scraped.to_csv('scraped_test' + '.csv')
 
 
 
     # browser.close()
 
-    scraped = pd.DataFrame(scraped)
-    # scraped.dropna(axis=0, inplace=True)
-    scraped.shape
-
-    scraped.to_csv('scraped' + str(batch) + '.csv')
 
 
-# provide insights geographcally, by business / economic class.
+
+
