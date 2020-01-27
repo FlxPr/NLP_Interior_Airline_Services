5fdf8f4f56b428c4f8100c5f8398199276d6321b
Felix <f.poirier.imt@gmail.com>
2020-01-27 17:38:00 +0100

    Add page browsing for efficiency

--- skytrax_parser.py
+++ skytrax_parser.py
@@ -5,7 +5,7 @@ from math import floor
 import time
 
 link_file_name = 'skytrax_reviews_links.txt'
-data_file_name = 'skytrax_reviews_data2.csv'
+data_file_name = 'skytrax_reviews_data.csv'
 
 # Makes selenium open Chrome in background
 chrome_options = Options()
@@ -26,9 +26,10 @@ def get_links():
                 link = airline.find_element_by_tag_name('a').get_attribute('href')
                 links.append(link)
     browser.close()
+    links = sorted(list(set(links)))
     # Save unique links in text file
     link_file = open(link_file_name, 'wt')
-    link_file.write('\n'.join(set(links)))
+    link_file.write('\n'.join(links))
 
 
 links = open(link_file_name).read().split('\n')
@@ -76,46 +77,57 @@ except FileNotFoundError:
 
 moritz_part = slice(340, 430)
 noel_part = slice(430, 520)
-felix_part = slice(570, 610)
+felix_part = slice(540, 560)
 shamir_part = slice(610, 700)
 
 browser = webdriver.Chrome(chrome_options=chrome_options)
+reviews_per_page = 50
 for link in links[felix_part]:
     tic = time.time()
-    browser.get(link + '/?sortby=post_date%3ADesc&pagesize=10000')  # show all reviews in one page
+    browser.get(link + '/?sortby=post_date%3ADesc&pagesize={}'.format(reviews_per_page))  # show all reviews in one page
     airline_name = browser.find_element_by_class_name('info').find_element_by_tag_name('h1').text
     review_type = browser.find_element_by_class_name('info').find_element_by_tag_name('h2').text
+    review_count = int(browser.find_element_by_class_name('review-count').text.split(' ')[-2])
     if review_type in data[data.airline == airline_name].review_type.unique():
         print('Already scraped {review_type} for {airline_name}'
               .format(review_type=review_type, airline_name=airline_name))
         continue
     print('Scraping {airline} {review}'.format(airline=airline_name, review=review_type))
-    reviews_container = browser.find_element_by_tag_name('article')
-    reviews = reviews_container.find_elements_by_tag_name('article')
+
     data_lines = []
-    for review in reviews:
-        data_line = {'airline': airline_name, 'review_type': review_type}
-        try:
-            data_line['rating'], data_line['best_rating'] = review.find_element_by_class_name('rating-10').text.split('/')
-        except ValueError:
-            pass
-        data_line['header'] = review.find_element_by_class_name('text_header').text
-        data_line['comment_date'] = review.find_element_by_tag_name('time').get_attribute('datetime')
-        data_line['comment'] = review.find_element_by_class_name('text_content').text
-        ratings_table = review.find_element_by_class_name('review-ratings').find_element_by_tag_name('tbody')
-        for rating in ratings_table.find_elements_by_tag_name('tr'):
+    number_of_pages = review_count//reviews_per_page + 1 if review_count % reviews_per_page != 0 \
+        else review_count//reviews_per_page
+    for page_number in range(1, number_of_pages + 1):
+
+        print('scraping page {} out of {}'.format(page_number, number_of_pages))
+        reviews_container = browser.find_element_by_tag_name('article')
+        reviews = reviews_container.find_elements_by_tag_name('article')
+        for review in reviews:
+            data_line = {'airline': airline_name, 'review_type': review_type}
             try:
-                key_value = rating.find_elements_by_tag_name('td')
-                if 'stars' in key_value[1].get_attribute('class'):
-                    number_of_stars = len(key_value[1].find_elements_by_class_name('star.fill '))
-                    data_line[key_value[0].text] = number_of_stars
-                else:
-                    data_line[key_value[0].text] = key_value[1].text
-            except Exception:
+                data_line['rating'], data_line['best_rating'] = review.find_element_by_class_name('rating-10').text.split('/')
+            except ValueError:
                 pass
-        data_lines.append(data_line)
+            data_line['header'] = review.find_element_by_class_name('text_header').text
+            data_line['comment_date'] = review.find_element_by_tag_name('time').get_attribute('datetime')
+            data_line['comment'] = review.find_element_by_class_name('text_content').text
+            ratings_table = review.find_element_by_class_name('review-ratings').find_element_by_tag_name('tbody')
+            for rating in ratings_table.find_elements_by_tag_name('tr'):
+                try:
+                    key_value = rating.find_elements_by_tag_name('td')
+                    if 'stars' in key_value[1].get_attribute('class'):
+                        number_of_stars = len(key_value[1].find_elements_by_class_name('star.fill '))
+                        data_line[key_value[0].text] = number_of_stars
+                    else:
+                        data_line[key_value[0].text] = key_value[1].text
+                except Exception:
+                    pass
+            data_lines.append(data_line)
+            time.sleep(0.5)
+        browser.get(link + '/page/{}/?sortby=post_date%3ADesc&pagesize={}'.format(page_number, reviews_per_page))
+
     data = data.append(pd.DataFrame(data_lines))
     data.to_csv(data_file_name, index=None)
     toc = time.time()
     print('Scraped {n_records} in {seconds} seconds'.format(n_records=len(data_lines), seconds=floor(toc-tic)))
-    time.sleep(0.5)
+    time.sleep(1)
