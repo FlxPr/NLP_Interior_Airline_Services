c9eaad819579ab247bf1dcf9011d062ff0dbe90c
Felix <f.poirier.imt@gmail.com>
2020-01-30 18:13:41 +0100

    Commit

---
+++ skytrax_scraper.py
@@ -0,0 +1,157 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+from math import floor
+import time
+
+link_file_name = 'skytrax_reviews_links.txt'
+data_file_name = 'skytrax_reviews_data.csv'
+reviews_per_page = 50  # Big number of reviews shown per page makes scraping inefficient
+
+# Makes selenium open Chrome in background
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+
+def get_links(file_path=link_file_name):
+    """
+    Gets all links to reviews per airline and per type for skytrax, sorts them and saves result to text file
+    :return: None
+    """
+    link_list = list()
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    for review_link in ['https://www.airlinequality.com/review-pages/a-z-{review_type}-reviews/'
+                        .format(review_type=review_type) for review_type in ('airline', 'seat')]:
+        browser.get(review_link)
+        airline_lists = [airline_list.find_elements_by_tag_name('li')
+                         for airline_list in browser.find_elements_by_class_name('items')]
+        for airline_list in airline_lists:
+            for airline in airline_list:
+                link = airline.find_element_by_tag_name('a').get_attribute('href')
+                link_list.append(link)
+    browser.close()
+
+    # Save sorted unique links in text file
+    link_list = sorted(list(set(link_list)))
+    link_file = open(file_path, 'wt')
+    link_file.write('\n'.join(link_list))
+
+    print("Scraped {} links to review pages from skytrax".format(len(link_list)))
+
+
+def scrap_review(review, airline_name, review_type):
+    """
+    scraps a single review block
+    :param review: WebDriver object containing the review
+    :param airline_name: name of the airline
+    :param review_type: type of review
+    :return: Dictionary containing scraping result
+    """
+    data_line = {'airline': airline_name, 'review_type': review_type,
+                 'header': review.find_element_by_class_name('text_header').text,
+                 'comment_date': review.find_element_by_tag_name('time').get_attribute('datetime'),
+                 'comment': review.find_element_by_class_name('text_content').text}
+    try:
+        data_line['rating'], data_line['best_rating'] = review.find_element_by_class_name('rating-10') \
+            .text.split('/')
+    except ValueError:
+        pass
+
+    ratings_table = review.find_element_by_class_name('review-ratings').find_element_by_tag_name('tbody')
+    for rating in ratings_table.find_elements_by_tag_name('tr'):
+        try:
+            key_value = rating.find_elements_by_tag_name('td')
+            if 'stars' in key_value[1].get_attribute('class'):
+                number_of_stars = len(key_value[1].find_elements_by_class_name('star.fill '))
+                data_line[key_value[0].text] = number_of_stars
+            else:
+                data_line[key_value[0].text] = key_value[1].text
+        except Exception:
+            pass
+    return data_line
+
+
+def scrap_page(browser, airline_name, review_type):
+    """
+    scraps a single page of reviews
+    :param browser: Web browser controlled by Selenium
+    :param airline_name: name of the airline
+    :param review_type: type of review
+    :param data_lines: list of dictionaries containing parsed reviews
+    :return: data_lines
+    """
+    reviews = browser.find_element_by_tag_name('article').find_elements_by_tag_name('article')
+    data_lines = []
+    for review in reviews:
+        data_lines.append(scrap_review(review, airline_name, review_type))
+    return data_lines
+
+
+def scrap_airline(link, browser, scraped_data):
+    """
+    scrap all the comments for a single airline and review type
+    :param link: Link to first page of skytrax reviews
+    :param browser: Browser controlled by Selenium
+    :param scraped_data: Already scrapd data for preventing redundant scraping
+    :return:
+    """
+    tic = time.time()
+    browser.get(link + '/?sortby=post_date%3ADesc&pagesize={}'.format(reviews_per_page))
+
+    airline_name = browser.find_element_by_class_name('info').find_element_by_tag_name('h1').text
+    review_type = browser.find_element_by_class_name('info').find_element_by_tag_name('h2').text
+    review_count = int(browser.find_element_by_class_name('review-count').text.split(' ')[-2])
+
+    if review_type in scraped_data[scraped_data.airline == airline_name].review_type.unique():
+        print('Already scraped {review_type} for {airline_name}'
+              .format(review_type=review_type, airline_name=airline_name))
+        return
+
+    print('Scraping {airline} {review}'.format(airline=airline_name, review=review_type))
+
+    data_lines = []
+    number_of_pages = review_count // reviews_per_page + 1 if review_count % reviews_per_page != 0 \
+        else review_count // reviews_per_page
+
+    for page_number in range(1, number_of_pages + 1):
+        print('scraping page {} out of {}'.format(page_number, number_of_pages))
+        data_lines.extend(scrap_page(browser, airline_name, review_type))
+        browser.get(link + '/page/{}/?sortby=post_date%3ADesc&pagesize={}'.format(page_number, reviews_per_page))
+
+    toc = time.time()
+    print('scraped {n_records} in {seconds} seconds'.format(n_records=len(data_lines), seconds=floor(toc - tic)))
+
+    scraped_data = scraped_data.append(pd.DataFrame(data_lines))
+    scraped_data.to_csv(data_file_name, index=None)
+    time.sleep(1)
+
+
+def scrap_skytrax(links_to_scrap):
+    """
+    scraps links provided from skytrax and saves results to a dataframe. Does not parse airline if already
+    present in the data frame.
+    :param links_to_scrap: list of links to pages of airline reviews
+    :return: None
+    """
+    try:
+        data = pd.read_csv(data_file_name, index_col=None)
+    except FileNotFoundError:
+        data = pd.DataFrame(columns=['airline', 'review_type'])
+        data.to_csv(data_file_name)
+
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    for link in links_to_scrap:
+        scrap_airline(link, browser, data)
+
+
+if __name__ == '__main__':
+    # TODO uncomment before delivering code to Eleven
+    # try:
+    #     links = open(link_file_name).read().split('\n')
+    # except FileNotFoundError:
+    #     print('Getting page links from skytrax')
+    #     get_links()
+    #     links = open(link_file_name).read().split('\n')
+
+    links = open(link_file_name).read().split('\n')
+    scrap_skytrax(links[3:4])
--- topic_modeling.py
+++ topic_modeling.py
@@ -3,6 +3,10 @@ import gensim
 import matplotlib.pyplot as plt
 from NLP_Interior_Airline_Services.data_cleaning import tokenize_lemma_stem
 import operator
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+from nltk.corpus import stopwords
 
 
 def get_coherence_values(corpus, texts, dictionary, start=4, stop=20, step=1):
@@ -13,103 +17,35 @@ def get_coherence_values(corpus, texts, dictionary, start=4, stop=20, step=1):
         model_list.append(model)
         coherencemodel = gensim.models.CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
         coherence_values.append(coherencemodel.get_coherence())
+    plot_coherence_values(coherence_values, start, stop, step)
     return model_list, coherence_values
 
 
 def plot_coherence_values(coherence_values, start, stop, step):
     plt.plot(range(start, stop, step), coherence_values)
-    plt.title('coherence values for different number of topics')
+    plt.title('Coherence values for different number of topics')
+    plt.xlabel('Number of topics')
+    plt.ylabel('Coherence score')
     plt.grid()
     plt.show()
 
 
-df = pd.read_csv('sentences_for_topic_modelling.csv')
-df['tokenized_sentences'] = df.sentence.apply(tokenize_lemma_stem)
+def print_most_important_topics_in_seat_reviews(topic_model):
+    full_comment_df = pd.read_csv('skytrax_reviews_data.csv', index_col=None)
+    seat_review_ids = full_comment_df[full_comment_df.review_type == 'Seat Reviews'].index
+    df_test = df[df.comment_id.isin(list(seat_review_ids))]
+    df_test['model_topics'] = df_test.bow_corpus.apply(topic_model.get_document_topics)
 
-bigram = gensim.models.Phrases(df['tokenized_sentences'], min_count=30)
-trigram = gensim.models.Phrases(bigram[df['tokenized_sentences']], min_count=20)
+    topic_importance = dict.fromkeys(range(num_topics), 0)
+    for model_topic in df_test.model_topics:
+        for topic in model_topic:
+            topic_importance[topic[0]] += topic[1]
 
-
-for idx in range(len(df.index)):
-    for token in bigram[df['tokenized_sentences'].iloc[idx]]:
-        if '_' in token:
-           df['tokenized_sentences'].iloc[idx].append(token)
-    for token in trigram[df['tokenized_sentences'].iloc[idx]]:
-        if token.count('_') == 2:
-           df['tokenized_sentences'].iloc[idx].append(token)
-
-# texts = [bigram[sentence] for sentence in df['tokenized_sentences']]
-
-# TOPIC MODEL --
-dictionary = gensim.corpora.Dictionary(df['tokenized_sentences'])
-dictionary.filter_extremes(no_below=10, no_above=0.10, keep_n=100000)  # TODO check no_above
-
-print('most common tokens: {}'.format(list(dictionary.items())[:10]))
-print('Number of unique tokens: %d' % len(dictionary))
-print('Number of documents: %d' % len(df.index))
-
-df['bow_corpus'] = df.tokenized_sentences.apply(lambda doc: dictionary.doc2bow(doc))
-
-tfidf = gensim.models.TfidfModel(list(df.bow_corpus))
-corpus_tfidf = tfidf[list(df.bow_corpus)]
-
-# PICK THE BEST MODEL ACCORDING TO ITS COHERENCE VALUE
-# model_list, coherence_values = get_coherence_values(dictionary, corpus_tfidf, dictionary)
-
-num_topics = 13
-
-# # Run LDA using TF-IDF
-# lda_model_tfidf = gensim.models.LdaMulticore(df['bow_corpus'], num_topics=num_topics, id2word=dictionary, passes=2, workers=4)
-# for idx, topic in lda_model_tfidf.print_topics(-1):
-#     print('Topic: {} Word: {}'.format(idx, topic))
-#
-
-# # Run LDA using TF-IDF
-# lda_model_tfidf = gensim.models.LdaMulticore(corpus_tfidf, num_topics=num_topics, id2word=dictionary, passes=2, workers=4)
-# for idx, topic in lda_model_tfidf.print_topics(-1):
-#     print('Topic: {} Word: {}'.format(idx, topic))
-
-# lda_model_tfidf.save('lda_model')
-# topic 0 : Seat comfort and recline
-# topic 7 : Seat comfort and extras
-# Topic 12 : Entertainment and food
+    print('Most important topic in Seat Reviews :\n{}'.format(
+        {k: v for k, v in sorted(topic_importance.items(), key=lambda item: item[1], reverse=True)}))
 
 
-# Tune number of topics hyperparameter with coherence metric (long to run)
-start = 4
-stop = 20
-step = 2
-models, coherence_values = get_coherence_values(df['bow_corpus'], df.tokenized_sentences, dictionary)
-
-best_models = sorted(list(zip(models, coherence_values)), key=operator.itemgetter(1), reverse=True)
-plot_coherence_values(coherence_values, start, stop, step)
-# Pick the interesting topics and test if they are preponderent in Seat Reviews:
-# Topics 0, 2, 9
-full_comment_df = pd.read_csv('skytrax_reviews_data.csv', index_col=None)
-seat_review_ids = full_comment_df[full_comment_df.review_type == 'Seat Reviews'].index
-
-df_test = df[df.comment_id.isin(list(seat_review_ids))]
-df_test['model_topics'] = df_test.bow_corpus.apply(best_models[0][0].get_document_topics)
-
-topic_importance = dict.fromkeys(range(num_topics), 0)
-for model_topic in df_test.model_topics:
-    for topic in model_topic:
-        topic_importance[topic[0]] += topic[1]
-
-print('Most important topic in Seat Reviews :\n{}'.format(
-    {k: v for k, v in sorted(topic_importance.items(), key=lambda item: item[1], reverse=True)}))
-
-
-# Classify sentences by topic
-lda_model_tfidf = gensim.models.LdaMulticore.load('lda_model')
-interesting_topics = [0, 7, 12]
-
-def plot_wordcloud():
-    # 1. Wordcloud of Top N words in each topic
-    from matplotlib import pyplot as plt
-    from wordcloud import WordCloud, STOPWORDS
-    import matplotlib.colors as mcolors
-    from nltk.corpus import stopwords
+def plot_wordcloud(): # TODO refactor because code taken from internet or delete
     stop_words = stopwords.words('english')
     stop_words.extend(
         ['from', 'subject', 're', 'edu', 'use', 'not', 'would', 'say', 'could', '_', 'be', 'know', 'good', 'go', 'get',
@@ -128,50 +64,97 @@ def plot_wordcloud():
                       prefer_horizontal=1.0)
 
     lda_model = gensim.models.LdaMulticore.load('lda_model')
-    topics = lda_model.show_topics(13, formatted=False)
-    topics = [topics[0], topics[7], topics[12]]
-    fig, axes = plt.subplots(1, 3, figsize=(10, 10))
+    topics = lda_model.show_topics(10, formatted=False)
 
-    topic_names = ('Comfort', 'Seat Room & Extras', 'Entertainment & Service')
-    for i, ax in enumerate(axes.flatten()):
-        fig.add_subplot(ax)
+    for idx, topic in lda_model_bag_of_words.print_topics(-1):
+        print(type(topic))
+        print('Topic: {} Word: {}'.format(idx, topic))
+
+    for i in range(len(topics)):
+        plt.figure()
         topic_words = dict(topics[i][1])
         cloud.generate_from_frequencies(topic_words, max_font_size=300)
         plt.gca().imshow(cloud)
-        plt.gca().set_title(topic_names[i], fontdict=dict(size=16))
         plt.gca().axis('off')
 
-    plt.subplots_adjust(wspace=0, hspace=0)
-    plt.axis('off')
-    plt.margins(x=0, y=0)
-    plt.tight_layout()
-    plt.show()
-
-df['sentence_topics'] = df.bow_corpus.apply(lda_model_tfidf.get_document_topics)
-df['sentence_best_topic'] = df.sentence_topics.apply(lambda x: max(x, key=operator.itemgetter(1))[0])
-df.to_csv('sentences_classified.csv')
+        plt.subplots_adjust(wspace=0, hspace=0)
+        plt.axis('off')
+        plt.margins(x=0, y=0)
+        plt.tight_layout()
+        plt.show()
 
 
-import pandas as pd
-df = pd.read_csv('sentences_classified.csv')
-interesting_topics = [0, 7, 12]
+if __name__ == '__main__':
+    df = pd.read_csv('noun_sentences_for_topic_modelling.csv')
+    df['comment_token_stem_lemma'] = df.noun.apply(tokenize_lemma_stem)
+    df = df.dropna(subset=['comment_token_stem_lemma'])
 
-d = dict.fromkeys(df.comment_id.unique())
-for comment_id in d.keys():
-    d[comment_id] = dict.fromkeys(interesting_topics)
-    for topic in interesting_topics:
-        d[comment_id][topic] = []
+    # Create bigrams and trigrams
+    bigram = gensim.models.Phrases(df['comment_token_stem_lemma'], min_count=30)
+    trigram = gensim.models.Phrases(bigram[df['comment_token_stem_lemma']], min_count=20)
+    for sentence_index in range(len(df.index)):
+        for token in bigram[df['comment_token_stem_lemma'].iloc[sentence_index]]:
+            if token.count('_') == 1:
+                df['comment_token_stem_lemma'].iloc[sentence_index].append(token)
+        for token in trigram[df['comment_token_stem_lemma'].iloc[sentence_index]]:
+            if token.count('_') == 2:
+                df['comment_token_stem_lemma'].iloc[sentence_index].append(token)
 
-count = 0
-for _, row in df.iterrows():
-    if row.sentence_best_topic in interesting_topics:
-        d[row.comment_id][row.sentence_best_topic].append(row.sentence)
-    count += 1
-    if count % 1000 == 0:
-        print(count)
+    # TOPIC MODEL - Create dictionary
+    dictionary = gensim.corpora.Dictionary(df['comment_token_stem_lemma'])
+    dictionary.filter_extremes(no_below=50, no_above=0.20, keep_n=100000)
 
-topic_df = pd.DataFrame(d).transpose()
-topic_names = {0: 'topic_comfort', 7: 'topic_extras', 12: 'topic_entertainment'}
-full_comment_df = full_comment_df.join(topic_df)
-full_comment_df = full_comment_df.rename(columns=topic_names)
-full_comment_df.to_csv('data_with_topics.csv')
+    num_topics = 10
+
+    # # Run LDA using Bag of Words
+    df['bow_corpus'] = df.comment_token_stem_lemma.apply(lambda doc: dictionary.doc2bow(doc))
+    lda_model_bag_of_words = gensim.models.LdaMulticore(df['bow_corpus'], num_topics=num_topics, id2word=dictionary, passes=2, workers=4)
+    for sentence_index, topic in lda_model_bag_of_words.print_topics(-1):
+       print('Topic: {} Word: {}'.format(sentence_index, topic))
+
+    # Run LDA using TF-IDF
+    tfidf = gensim.models.TfidfModel(list(df.bow_corpus))
+    corpus_tfidf = tfidf[list(df.bow_corpus)]
+    lda_model_tfidf = gensim.models.LdaMulticore(corpus_tfidf, num_topics=num_topics, id2word=dictionary, passes=2, workers=4)
+    for idx, topic in lda_model_tfidf.print_topics(-1):
+        print('Topic: {} Word: {}'.format(idx, topic))
+
+    # Tune number of topics hyperparameter with coherence metric (long to run)
+    start, stop, step = 20, 30, 1
+    models, coherence_values = get_coherence_values(df['bow_corpus'], df.comment_token_stem_lemma, dictionary)
+    plot_coherence_values(coherence_values, start, stop, step)
+    best_models = sorted(list(zip(models, coherence_values)), key=operator.itemgetter(1), reverse=True)
+
+    # Test topic modeling by predicting topics on 'Seat Reviews of Skytrax which are aircraft interior related
+    print_most_important_topics_in_seat_reviews(best_models[0][0])
+    lda_model_bag_of_words.save('Models/lda_model_bag_of_words')
+
+    # Classify sentences by topic
+    lda_model_bag_of_words = gensim.models.LdaMulticore.load('lda_model')
+    interesting_topics = [0, 7, 12]
+
+    df['sentence_topic'] = df.bow_corpus.apply(lda_model_bag_of_words.get_document_topics)
+    df.to_csv('sentences_classified.csv')
+
+    df = pd.read_csv('sentences_classified.csv')
+    interesting_topics = [0, 7, 12]
+
+    d = dict.fromkeys(df.comment_id.unique())
+    for comment_id in d.keys():
+        d[comment_id] = dict.fromkeys(interesting_topics)
+        for topic in interesting_topics:
+            d[comment_id][topic] = []
+
+    count = 0
+    for _, row in df.iterrows():
+        if row.sentence_best_topic in interesting_topics:
+            d[row.comment_id][row.sentence_best_topic].append(row.sentence)
+        count += 1
+        if count % 1000 == 0:
+            print(count)
+
+    topic_df = pd.DataFrame(d).transpose()
+    topic_names = {0: 'topic_comfort', 7: 'topic_extras', 12: 'topic_entertainment'}
+    full_comment_df = full_comment_df.join(topic_df)
+    full_comment_df = full_comment_df.rename(columns=topic_names)
+    full_comment_df.to_csv('data_with_topics.csv')
